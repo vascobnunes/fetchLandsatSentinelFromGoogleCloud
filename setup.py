fd1b01c1d5af225f05ffa8d5cf6f8bea328363c0
David Ketchum <dgketchum@users.noreply.github.com>
2018-04-08 18:42:14 -0600

    update setup.py
    This should allow one to download the package from pip, and use the `fels` command, rather than typing out the file name.
    
    This is based on my own (USGS-based) landsat downloader https://github.com/dgketchum/Landsat578/blob/master/setup.py that I have operational on pip.  USGS was down today and I started to work on building gcs functionality and found your project!
    
    A great resource describing how to push new 'tags' (aka versions) to pip is here:  http://peterdowns.com/posts/first-time-with-pypi.html.

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+
+    setup_kwargs = {'entry_points': {'console_scripts': ['fels=fetchFromGoogleCloud:main']}}
+except ImportError:
+    from distutils.core import setup
+
+    setup_kwargs = {'scripts': ['bin/fels']}
+
+tag = '1.3.0'
 
 setup(name='FeLS',
-      version='1.2.9',
+      version=tag,
+      py_modules=['fels'],
       description='Fetch Landsat & Sentinel Data from google cloud',
       url='https://github.com/vascobnunes/fetchLandsatSentinelFromGoogleCloud',
       author='vascobnunes',
       author_email='vascobnunes@gmail.com',
       license='GPL',
       zip_safe=False,
+      packages=['fels'],
       install_requires=['numpy'],
-      dependency_links=['https://www.conan.io/source/Gdal/2.1.3/osechet/stable'])
+      dependency_links=['https://www.conan.io/source/Gdal/2.1.3/osechet/stable'],
+      **setup_kwargs)
