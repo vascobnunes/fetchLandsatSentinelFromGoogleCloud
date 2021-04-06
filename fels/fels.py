ad1ef51624f23e0751d0752d34ed2e8ccfff69a6
Matthew <matthew.bernstein@kitware.com>
2021-04-06 10:56:23 -0400

    add python interface

--- fels/fels.py
+++ fels/fels.py
@@ -336,7 +336,7 @@ def landsatdir_to_date(string, processing=False):
     return date(*d)
 
 
-def main():
+def get_parser():
     parser = argparse.ArgumentParser(description="Find and download Landsat and Sentinel-2 data from the public Google Cloud")
     parser.add_argument("scene", help="WRS2 coordinates of scene (ex 198030)")
     parser.add_argument("sat", help="Which satellite are you looking for", choices=['TM', 'ETM', 'OLI_TIRS', 'S2'])
@@ -349,11 +349,92 @@ def main():
     parser.add_argument("--noinspire", help="Do not rename output image folder to the title collected from the inspire.xml file (only for S2 datasets)", action="store_true", default=False)
     parser.add_argument("--outputcatalogs", help="Where to download metadata catalog files", default=None)
     parser.add_argument("--overwrite", help="Overwrite files if existing locally", action="store_true", default=False)
-    parser.add_argument("-l", "--list", help="List available download url's and exit without downloading", action="store_true", default=False)
-    options = parser.parse_args()
+    parser.add_argument("-l", "--list", help="List available download urls and exit without downloading", action="store_true", default=False)
+    return parser
+
+
+def main():
+    '''
+    CLI entrypoint.
+    '''
+    options = get_parser().parse_args()
 
     if not options.outputcatalogs:
         options.outputcatalogs = options.output
+    
+    url = _run_fels(options)
+
+    if options.list:
+        for u in url:
+            print(u)
+
+
+def run_fels(*args, **kwargs):
+    '''
+    Python entrypoint.
+
+    See main() for arguments. Additional options not present in argparse include
+    Args:
+        scene: for Landsat, can pass in a (path,row) tuple such as (115,34)
+        sat: 'L5', 'L7', 'L8' are aliases for 'TM', 'ETM', 'OLI_TIRS'
+        start_date: can pass in a datetime.date directly
+        end_date: can pass in a datetime.date directly
+
+    Other differences from CLI:
+        Returns the list of urls. Therefore, will not print them with list=True.
+
+    Example:
+        >>> # downloading a tile from the CLI
+        >>> os.system('fels 203031 OLI_TIRS 2015-01-01 2015-06-30 -c 30 -o . --latest --outputcatalogs ~/data/fels/')
+        >>> # downloading the same tile in Python
+        >>> from datetime import date
+        >>> from fels import run_fels
+        >>> run_fels((203,31), 'L8', date(2015, 1, 1), date(2015, 6, 30), cloudcover=30, output='.', latest=True, outputcatalogs=os.path.expanduser('~/data/fels/'))
+    '''
+
+    assert len(args) == 4
+    scene, sat, start_date, end_date = args
+    
+    # fix alternate args
+
+    if isinstance(scene, tuple):
+        assert len(scene) == 2
+        scene = str(scene[0]).zfill(3) + str(scene[1]).zfill(3)
+
+    landsats = {
+            'L5': 'TM',
+            'L7': 'ETM',
+            'L8': 'OLI_TIRS'
+            }
+    if sat in landsats:
+        sat = landsats[sat]
+    
+    if isinstance(end_date, datetime.date):
+        end_date = end_date.strftime('%Y-%m-%d')
+    if isinstance(start_date, datetime.date):
+        start_date = start_date.strftime('%Y-%m-%d')
+
+    # get defaults from argparse
+
+    defaults = get_parser().parse_args([scene, sat, start_date, end_date])
+
+    # overwrite with user-defined kwargs
+
+    for k in kwargs:
+        assert k in defaults, k
+
+    options_dict = vars(defaults)
+    options_dict.update(kwargs)
+    options = argparse.Namespace(**options_dict)
+
+    # call fels
+    return _run_fels(options)
+
+
+def _run_fels(options):
+    '''
+    Search the catalogs for matching images, download them (if list==False) and return the list of urls.
+    '''
 
     LANDSAT_METADATA_URL = 'http://storage.googleapis.com/gcp-public-data-landsat/index.csv.gz'
     SENTINEL2_METADATA_URL = 'http://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz'
@@ -370,8 +451,6 @@ def main():
                 if not options.list:
                     print("Downloading {} of {}...".format(i+1, len(url)))
                     get_sentinel2_image(u, options.output, options.overwrite, options.excludepartial, options.noinspire)
-                else:
-                    print(url[i])
     else:
         landsat_metadata_file = download_metadata_file(LANDSAT_METADATA_URL, options.outputcatalogs, 'Landsat')
         url = query_landsat_catalogue(landsat_metadata_file, options.cloudcover, options.start_date,
@@ -385,9 +464,8 @@ def main():
                 if not options.list:
                     print("Downloading {} of {}...".format(i+1, len(url)))
                     get_landsat_image(u, options.output, options.overwrite, options.sat)
-                else:
-                    print(url[i])
 
+    return url
 
 if __name__ == "__main__":
     main()
